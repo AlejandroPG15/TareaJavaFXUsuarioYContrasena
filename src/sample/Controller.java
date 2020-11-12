2345a47f8ef50917a8f99f90b5e7c38d3b71204b
Alejandro <alejandropg140600@hotmail.com>
2020-11-12 14:33:01 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+
+    Usuario usuario = new Usuario();
+
+    @FXML
+    private Label idLabelMostrar;
+
+   @FXML
+    public void onMostarEntered(MouseEvent event){
+       idLabelMostrar.setText(usuario.contrasena.toString());
+    }
+
+}
