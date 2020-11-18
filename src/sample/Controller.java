392a6303091a70b88e8f7a1d195be4c2d9cf539a
Alejandro <alejandropg140600@hotmail.com>
2020-11-18 11:12:14 +0100

    Second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,18 +2,25 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
-    Usuario usuario = new Usuario();
+    @FXML
+    private PasswordField idTextContrasena;
 
     @FXML
     private Label idLabelMostrar;
 
    @FXML
     public void onMostarEntered(MouseEvent event){
-       idLabelMostrar.setText(usuario.contrasena.toString());
+       idLabelMostrar.setText(idTextContrasena.getText());
+    }
+
+    @FXML
+    public void onMostarExited(MouseEvent event){
+        idLabelMostrar.setText("Mostrar Contraseña");
     }
 
 }
